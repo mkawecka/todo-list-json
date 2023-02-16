96703f61a5713fa16f3287dde7ee09fc255f8b36
Magdalena Kawecka <wilczynska.nysa@gmail.com>
2023-02-16 20:49:15 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,132 @@
+let todoInput;
+let errorInfo;
+let addBtn;
+let ulList;
+let newTodo;
+let popup;
+let popupInfo;
+let todoToEdit;
+let popupInput;
+let popupAddBtn;
+let popupCloseBtn;
+
+const main = () => {
+	prepareDOMElements();
+	prepareDOMEvents();
+};
+
+const prepareDOMElements = () => {
+	todoInput = document.querySelector('.todo-input');
+	errorInfo = document.querySelector('.error-info');
+	addBtn = document.querySelector('.btn-add');
+	ulList = document.querySelector('.todolist ul');
+	popup = document.querySelector('.popup');
+	popupInfo = document.querySelector('.popup-info');
+	popupInput = document.querySelector('.popup-input');
+	popupAddBtn = document.querySelector('.accept');
+	popupCloseBtn = document.querySelector('.cancel');
+	categorySelect = document.querySelector('#task-category');
+	categoryOptions = document.querySelectorAll('.category-option');
+	categoryIcons = document.querySelectorAll('.category-icon');
+};
+
+const prepareDOMEvents = () => {
+	addBtn.addEventListener('click', addNewTodo);
+	ulList.addEventListener('click', checkClick);
+	popupCloseBtn.addEventListener('click', closePopup);
+	popupAddBtn.addEventListener('click', changeTodoText);
+	todoInput.addEventListener('keyup', enterKeycheck);
+};
+
+const addNewTodo = () => {
+	errorInfo.textContent = '';
+
+	if (todoInput.value !== '' && categorySelect.value !== '0') {
+		newTodo = document.createElement('li');
+		const taskPanel = document.createElement('div');
+		taskPanel.classList.add('task');
+		taskPanel.innerHTML = `<i class="category-icon fas fa-${categorySelect.value}"></i> ${todoInput.value}`;
+
+		newTodo.append(taskPanel);
+
+		createToolsArea(newTodo);
+		ulList.append(newTodo);
+
+		todoInput.value = '';
+		errorInfo.textContent = '';
+	} else if (todoInput.value !== '' && categorySelect.value === '0') {
+		errorInfo.textContent = 'Select task category!';
+	} else if (todoInput.value === '' && categorySelect.value !== '0') {
+		errorInfo.textContent = 'Enter task name!';
+	} else {
+		errorInfo.textContent = 'Enter task name and select task category!';
+	}
+};
+
+const createToolsArea = () => {
+	const toolsPanel = document.createElement('div');
+	toolsPanel.classList.add('tools');
+	newTodo.append(toolsPanel);
+
+	const completeBtn = document.createElement('button');
+	completeBtn.classList.add('complete');
+	completeBtn.innerHTML = '<i class="fas fa-check"></i>';
+
+	const editBtn = document.createElement('button');
+	editBtn.classList.add('edit');
+	editBtn.textContent = 'EDIT';
+
+	const deleteBtn = document.createElement('button');
+	deleteBtn.classList.add('delete');
+	deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
+
+	toolsPanel.append(completeBtn, editBtn, deleteBtn);
+};
+
+const checkClick = e => {
+	if (e.target.matches('.complete')) {
+		e.target.closest('li').classList.toggle('completed');
+		e.target.classList.toggle('completed');
+	} else if (e.target.matches('.edit')) {
+		editTodo(e);
+	} else if (e.target.matches('.delete')) {
+		deleteTodo(e);
+	}
+};
+
+const editTodo = e => {
+	todoToEdit = e.target.closest('li');
+	popupInput.value = todoToEdit.firstChild.textContent;
+	popup.style.display = 'flex';
+};
+
+const closePopup = () => {
+	popup.style.display = 'none';
+};
+
+const changeTodoText = () => {
+	if (popupInput.value !== '') {
+		todoToEdit.firstChild.textContent = popupInput.value;
+		popup.style.display = 'none';
+		popupInfo.textContent = ' ';
+	} else {
+		popupInfo.textContent = 'Podaj treść';
+	}
+};
+const deleteTodo = e => {
+	e.target.closest('li').remove();
+
+	const allTodos = ulList.querySelectorAll('li');
+
+	if (allTodos.length === 0) {
+		errorInfo.textContent = 'No task to do ;)';
+	}
+};
+
+const enterKeycheck = e => {
+	if (e.key === 'Enter') {
+		addNewTodo();
+	}
+};
+
+document.addEventListener('DOMContentLoaded', main);
